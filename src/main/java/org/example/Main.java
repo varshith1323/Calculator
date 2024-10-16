78b811b908f491f323aa81af541d4d57ab5a2577
Varshith Vattikuti <varshithvattikuti@Varshiths-MacBook-Pro.local>
2024-10-16 17:39:41 +0530

    hi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
