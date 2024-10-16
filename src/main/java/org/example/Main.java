215ba74ce137eeb04350eadfd70aece292875d99
Varshith Vattikuti <varshithvattikuti@Varshiths-MacBook-Pro.local>
2024-10-16 18:31:02 +0530

    hi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.Scanner;
 
 public class Main {
-
+//hi
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
